68eddffe84d64cadba7b07dced2d07d5569d9206
aemartos <aemartos@gmail.com>
2018-03-20 11:57:03 +0100

    URL nav position set

--- app/components/ModalGameStart.jsx
+++ app/components/ModalGameStart.jsx
@@ -29,7 +29,7 @@ export default class ModalGameStart extends React.Component {
                             <Icon className="control control_plus" icon="plus2"/>
                           </div>
                           <ul className={this.state.show_items ? "":"hide"}>
-                            {this.props.questions.map((q,index) => {return <li key={index}>{q.source_name}</li>;})
+                            {this.props.questions.map((q,index) => {return <li key={index}>- {q.source_name}</li>;})
                             }
                           </ul>
                        </div>
