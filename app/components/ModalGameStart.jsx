16ac80b30f3349fbafd9b517062aebaa8ae99cd0
ebarra <ebarra@dit.upm.es>
2018-03-21 09:52:43 +0100

    reset button info removed from modals

--- app/components/ModalGameStart.jsx
+++ app/components/ModalGameStart.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Modal from './Modal.jsx';
 import Icon from './Icon.jsx';
+import {UI} from '../config/config';
 
 export default class ModalGameStart extends React.Component {
   constructor(props){
@@ -36,7 +37,8 @@ export default class ModalGameStart extends React.Component {
                        <ul className="icon_list">
                         <li><Icon className="control control_info" icon="info"/>muestra las instrucciones</li>
                         <li><Icon className="control control_progress" icon="progress"/>muestra el progreso</li>
-                        <li><Icon className="control control_reset" icon="reset"/>reinicia la prueba</li>
+                        {UI.with_reset_button &&
+                          <li><Icon className="control control_reset" icon="reset"/>reinicia la prueba</li>}
                         <li><Icon className="control control_stop" icon="stop"/>finaliza la prueba</li>
                        </ul>
                      </div>
